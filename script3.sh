579fba930c940b6af886135291b79de34c8eb930
aashisingh10308 <aashi.24bsa10308@vitbhopal.ac.in>
2026-03-31 23:46:17 +0530

    Create script3.sh

---
+++ script3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Script 3: Disk and Permission Auditor
+
+DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
+
+echo "Directory Audit Report"
+echo "----------------------"
+
+for DIR in "${DIRS[@]}"; do
+    if [ -d "$DIR" ]; then
+        PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
+        SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
+        echo "$DIR => Permissions: $PERMS | Size: $SIZE"
+    else
+        echo "$DIR does not exist"
+    fi
+done
+
+# MySQL config directory check
+CONFIG="/etc/mysql"
+if [ -d "$CONFIG" ]; then
+    echo "MySQL Config found:"
+    ls -ld $CONFIG
+else
+    echo "MySQL config directory not found"
+fi
